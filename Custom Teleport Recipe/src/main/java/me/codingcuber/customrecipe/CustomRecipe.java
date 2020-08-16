b3fea4e69a93a253e0ebd353842b16b6914239d9
LachlynR <lachyreynolds@outlook.com>
2020-08-16 16:06:20 +1000

    Added Boilerplate for Teleport Custom Recipe

---
+++ Custom Teleport Recipe/src/main/java/me/codingcuber/customrecipe/CustomRecipe.java
@@ -0,0 +1,17 @@
+package me.codingcuber.customrecipe;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CustomRecipe extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
